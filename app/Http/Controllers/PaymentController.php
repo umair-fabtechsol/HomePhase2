db371ec6dd240976b10d28ca77344b03af2edec5
sagar-fabtechsol <sagar.sabir@fabtechsol.com>
2025-03-01 16:12:20 +0500

    update deals query.

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Stripe\Stripe;
+use Stripe\Payout;
+use Stripe\Balance;
 class PaymentController extends Controller
 {
-    //
+    public function createPayout(Request $request)
+    {
+        try {
+            Stripe::setApiKey(config('services.stripe.secret'));
+            $payout = Payout::create([
+                'amount' => $request->amount * 100,
+                'currency' => 'usd',
+                'method' => 'standard',
+            ]);
+            return response()->json(['success' => true, 'payout' => $payout]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+    
+    public function checkBalance()
+{
+    try {
+        Stripe::setApiKey(config('services.stripe.secret'));
+
+        $balance = Balance::retrieve();
+        return response()->json(['balance' => $balance]);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
+    }
+}
 }
