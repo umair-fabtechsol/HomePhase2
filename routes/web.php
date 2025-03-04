684babca6fa9a9e2d43a5e05f71afd086d7a7137
AliHassanFabtechsol <alihassandaska@fabtechsol.com>
2025-03-04 12:12:28 +0500

    test charge stripe payment

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,13 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('form', [App\Http\Controllers\AuthController::class, 'form'])->name('form');
 Route::post('MediaUpload', [App\Http\Controllers\ServiceProviderController::class, 'MediaUpload'])->name('TestMediaUpload');
+
+Route::get('/payment', [PaymentController::class, 'index']);
+Route::post('/charge', [PaymentController::class, 'charge'])->name('stripe.charge');
+Route::get('/checkout', [PaymentController::class, 'checkout'])->name('stripe.checkout');
+Route::get('/payment-success', function () {
+    return "Payment successful!";
+});
+Route::get('/cancel', function () {
+    return "Payment canceled!";
+});
