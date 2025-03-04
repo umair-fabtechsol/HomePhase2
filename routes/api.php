05ae6dc6174b1794e1945683b2920da695df0da2
shakaib-fabtechsol <shakaib@fabtechsol.com>
2025-03-04 17:50:33 +0500

    Add contact route to API for handling contact requests

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::controller(AuthController::class)->group(function () {
     Route::get('auth/google/callback', 'googleHandle');
     Route::get('facebookLogin', 'facebookLogin')->name('facebookLogin');
     Route::get('auth/facebook/callback', 'facebookHandle');
+    Route::get('contact', 'contact');
 
 
     
