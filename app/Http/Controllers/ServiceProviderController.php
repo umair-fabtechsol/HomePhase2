4c9e3b0746da2bcd9b040d1ad400ad28e483b414
AliHassanFabtechsol <alihassandaska@fabtechsol.com>
2025-03-08 16:46:04 +0500

    update

--- app/Http/Controllers/ServiceProviderController.php
+++ app/Http/Controllers/ServiceProviderController.php
@@ -1664,7 +1664,7 @@ class ServiceProviderController extends Controller
             return response()->json(['message' => 'You are not authorized'], 401);
         }
     }
-
+    // for home page for provider and customer 
     public function FilterHomeDeals(Request $request)
     {
         $role = Auth::user()->role;
@@ -1680,7 +1680,7 @@ class ServiceProviderController extends Controller
             ->orderBy('deals.id', 'desc')
             ->select('deals.*', 'users.name as user_name', 'users.personal_image', 'reviews.rating as review_rating');
             if($service){
-                $deals = $deals->where('deals.service_category', $service);
+                $deals = $deals->where('service_category', 'like', '%' . $request->service . '%');
             }
             if($reviews){
                 $deals = $deals->where('reviews.rating', $reviews);
@@ -1706,7 +1706,7 @@ class ServiceProviderController extends Controller
             }
 
             if($location){
-                $locationDistance = BusinessProfile::where('service_location', '<=', $location)->pluck('user_id')->toArray();
+                $locationDistance = BusinessProfile::where('service_location', 'like', '%' . $request->location . '%')->pluck('user_id')->toArray();
                 $deals = $deals->whereIn('deals.user_id', $locationDistance);
             }
 
@@ -1752,6 +1752,7 @@ class ServiceProviderController extends Controller
         
     }
 
+    // for home page for provider and customer 
     public function RecentViewDeals(Request $request){
         $role = Auth::user()->role;
 
