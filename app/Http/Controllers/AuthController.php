ed6f6d8413a765b34bff22f8f8fdf243914c2562
sagar-fabtechsol <sagar.sabir@fabtechsol.com>
2025-03-12 17:37:21 +0500

    update login with google api.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -144,9 +144,7 @@ class AuthController extends Controller
         if($getuser){
         Auth::login($getuser);
         $token = $getuser->createToken('auth_token')->plainTextToken;
-        if ($getuser->role == 2) {
-            return redirect()->away('https://homerservice-ph2.netlify.app/provider/services');
-        }
+        
     }else{
 
         return [
