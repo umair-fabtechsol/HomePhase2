d573e6c77ea18e659f1e27894ce99a8d4cbf1d50
shakaib-fabtechsol <shakaib@fabtechsol.com>
2025-05-07 12:06:39 +0500

    Refactor searchBusiness method to include optional service filtering and improve query structure; update validation rules and response messages.

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -250,13 +250,81 @@ class CommonController extends Controller
         }
         return response()->json(['reviews' => $reviews], 200);
     }
+    // public function searchBusiness(Request $request)
+    // {
+    //     $request->validate([
+    //         'search_address' => 'required|json',
+    //     ]);
+
+    //     $searchAddress = $request->input('search_address');
+
+    //     $geocode = file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=" . urlencode($searchAddress) . "&key=AIzaSyAu1gwHCSzLG9ACacQqLk-LG8oJMkarNF0");
+    //     $geocode = json_decode($geocode);
+
+    //     if (isset($geocode->results[0])) {
+    //         $addressComponents = $geocode->results[0]->address_components;
+    //         $country = null;
+    //         $province = null;
+    //         $city = null;
+    //         $zip = null;
+
+    //         foreach ($addressComponents as $component) {
+    //             if (in_array('country', $component->types)) {
+    //                 $country = $component->long_name;
+    //             }
+    //             if (in_array('administrative_area_level_1', $component->types)) {
+    //                 $province = $component->long_name;
+    //             }
+    //             if (in_array('locality', $component->types)) {
+    //                 $city = $component->long_name;
+    //             }
+    //             if (in_array('postal_code', $component->types)) {
+    //                 $zip = $component->long_name;
+    //             }
+    //         }
+
+    //         $query = BusinessProfile::query();
+
+    //         if ($country) {
+    //             $query->where('business_location', 'LIKE', '%' . $country . '%');
+    //         }
+
+    //         if ($province) {
+    //             $query->where('business_location', 'LIKE', '%' . $province . '%');
+    //         }
+
+    //         if ($city) {
+    //             $query->where('business_location', 'LIKE', '%' . $city . '%');
+    //         }
+
+    //         if ($zip) {
+    //             $query->where('business_location', 'LIKE', '%' . $zip . '%');
+    //         }
+
+    //         $businesses = $query->pluck('user_id');
+
+
+    //         $deals = Deal::whereIn('user_id', $businesses)->get();
+
+    //         return response()->json([
+    //             'businesses' => $businesses,
+    //             'deals' => $deals,
+    //         ]);
+    //     } else {
+    //         return response()->json([
+    //             'no data found'
+    //         ]);
+    //     }
+    // }
     public function searchBusiness(Request $request)
     {
         $request->validate([
-            'search_address' => 'required|json',
+            'search_address' => 'required|string',
+            'service' => 'nullable|string', // new input
         ]);
 
         $searchAddress = $request->input('search_address');
+        $searchService = $request->input('service');
 
         $geocode = file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=" . urlencode($searchAddress) . "&key=AIzaSyAu1gwHCSzLG9ACacQqLk-LG8oJMkarNF0");
         $geocode = json_decode($geocode);
@@ -285,26 +353,26 @@ class CommonController extends Controller
 
             $query = BusinessProfile::query();
 
-            if ($country) {
-                $query->where('business_location', 'LIKE', '%' . $country . '%');
-            }
-
-            if ($province) {
-                $query->where('business_location', 'LIKE', '%' . $province . '%');
-            }
-
-            if ($city) {
-                $query->where('business_location', 'LIKE', '%' . $city . '%');
-            }
-
-            if ($zip) {
-                $query->where('business_location', 'LIKE', '%' . $zip . '%');
-            }
+            $query->where(function ($q) use ($country, $province, $city, $zip) {
+                if ($country) $q->orWhere('business_location', 'LIKE', '%' . $country . '%');
+                if ($province) $q->orWhere('business_location', 'LIKE', '%' . $province . '%');
+                if ($city) $q->orWhere('business_location', 'LIKE', '%' . $city . '%');
+                if ($zip) $q->orWhere('business_location', 'LIKE', '%' . $zip . '%');
+            });
 
             $businesses = $query->pluck('user_id');
 
+            $dealsQuery = Deal::whereIn('user_id', $businesses);
 
-            $deals = Deal::whereIn('user_id', $businesses)->get();
+            if (!empty($searchService)) {
+                $dealsQuery->where(function ($q) use ($searchService) {
+                    $q->where('service_title', 'LIKE', '%' . $searchService . '%')
+                        ->orWhere('service_category', 'LIKE', '%' . $searchService . '%')
+                        ->orWhere('search_tags', 'LIKE', '%' . $searchService . '%');
+                });
+            }
+
+            $deals = $dealsQuery->get();
 
             return response()->json([
                 'businesses' => $businesses,
@@ -312,8 +380,8 @@ class CommonController extends Controller
             ]);
         } else {
             return response()->json([
-                'no data found'
-            ]);
+                'message' => 'No data found.',
+            ], 404);
         }
     }
 }
